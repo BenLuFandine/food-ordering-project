3c5d540cb6bbbbf94b9c89720d2d4ad99da735cb
Ben Lu <junqing.lu@fandine.com>
2015-09-21 04:54:36 -0700

    Initial set up

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name foodOrderingProjectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the foodOrderingProjectApp
+ */
+angular.module('foodOrderingProjectApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name foodOrderingProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the foodOrderingProjectApp
+ */
+angular.module('foodOrderingProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
